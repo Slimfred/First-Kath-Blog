b58668c81c74723d8257a1c043f7d85591004db6
Slimfred <slimfred37@gmail.com>
2017-02-17 12:33:49 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  root "main_page#index"
+  get 'main_page/index'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end

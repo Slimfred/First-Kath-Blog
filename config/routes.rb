b3e5674960bbbcffd9a184b3268ba49745bf1f91
Slimfred <slimfred37@gmail.com>
2017-02-24 17:39:41 -0500

    add nav bar, about view, contact view, add gem pg

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :posts
+
   root "main_page#index"
-  get 'main_page/index'
+
+  get "main_page/index"
+  get "main_page/about"
+  get "main_page/contact"
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

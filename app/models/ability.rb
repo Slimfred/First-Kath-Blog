67e92b4ab3f87f2a6a5bffe6c511a8aa23da9c86
Slimfred <slimfred37@gmail.com>
2017-03-01 16:58:36 -0500

    add admin to ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -29,15 +29,15 @@ class Ability
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
 
-    # user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :manage, User, id: user.id
-    #     can :read, Post
-    #   end
-
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+      if user.admin?
+        can :manage, :all
+      else
+        can :manage, User, id: user.id
+        can :read, Post
+      end
+
+    # user ||= User.new # guest user (not logged in)
+    # can :manage, User, id: user.id
   end
 end

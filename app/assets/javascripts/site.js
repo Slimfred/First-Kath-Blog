5ad5979ca665989d67b584f61d417980e3c44600
Slimfred <slimfred37@gmail.com>
2017-03-01 14:36:16 -0500

    add cancancan gem, set admin user, restrict new and edit post to admin

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,7 @@
+$(document).on('turbolinks:load', function(){
+
+  $('.alert').delay(2000).fadeOut(1500);
+
+  $('#notice').delay(2000).fadeOut(1500);
+
+)};
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
 
+  validates :subject, presence: true
+  validates :title, presence: true
+  validates :body, presence: true
+
   scope :created_at_asc, -> {order(created_at: :asc)}
 
 end
